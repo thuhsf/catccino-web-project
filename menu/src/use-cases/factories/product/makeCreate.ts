a01f391909b13df554799ee54cd45233d7d28fc1
thuanny helen <thuannyhelen@hotmail.com>
2026-05-09 16:17:40 -0300

    Update makeCreate.ts

--- menu/src/use-cases/factories/product/makeCreate.ts
+++ menu/src/use-cases/factories/product/makeCreate.ts
@@ -1,9 +1,11 @@
 import type { IProductFactory } from "@repositories/interfaces/IProductRepository.js";
 import CreateProductUseCase from "../../product/CreateProductUseCase.js";
+import type { ICategoryFactory } from "@repositories/interfaces/ICategoryRepository.js";
 
-export function makeCreate(productFactory: IProductFactory) {
-	const factory = productFactory.createRepository();
-	const service = new CreateProductUseCase(factory);
+export function makeCreate(productFactory: IProductFactory, categoryFactory: ICategoryFactory) {
+	const productFact = productFactory.createRepository();
+	const categoryFact = categoryFactory.createRepository();
+	const service = new CreateProductUseCase(productFact, categoryFact);
 
 	return service;
 }
